1dfe380cde67ab657cb2c31dcc441b3bf77c3fe7
RafaElVato <00118519@uca.edu.sv>
2020-04-17 21:07:08 -0600

    Change: Project name

---
+++ RafaelParcial/src/com/RHFA_00118519/x/CalculadoraImpuestos.java
@@ -0,0 +1,21 @@
+package com.RHFA_00118519.x;
+
+public class CalculadoraImpuestos {
+
+    private static double totalRenta;
+    private static double totalISSS;
+    private static double totalAFP;
+
+    private CalculadoraImpuestos() {
+
+    }
+
+    public static double calcularPago(Empleado pisto) {
+
+    }
+
+    public static String mostrarTotales() {
+
+    }
+
+}
